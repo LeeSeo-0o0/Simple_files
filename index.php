d7a69a81d9e80f04fa73664f72285a0122a79e0c
LeeSeo-0o0 <acorn0987@naver.com>
2024-08-25 12:16:07 +0900

    Update index.php
    렉토리 트래버설 방지: realpath()를 사용하여 현재 디렉토리와 기본 디렉토리의 실제 경로를 확인하고, 기본 디렉토리 밖으로 나가는 경로가 입력될 경우 이를 차단했습니다.
    
    XSS 방지: htmlspecialchars() 함수를 사용해 사용자 입력 값을 출력할 때 특수 문자를 이스케이프 처리하여 XSS 공격을 방지했습니다.
    
    파일 경로 인코딩: 파일 경로를 URL에 포함할 때 urlencode()를 사용하여 안전하게 인코딩하였으며, copyDownloadLink 함수에서 encodeURI를 사용하여 링크가 올바르게 처리되도록 했습니다.
    
    민감한 파일 숨기기: index.php와 index.html 파일을 목록에서 제외하여 민감한 파일이 노출되지 않도록 했습니다.

--- index.php
+++ index.php
@@ -70,8 +70,13 @@
 <div class="container">
     <h1 class="mt-4">파일 및 폴더 목록</h1>
     <?php
-    $baseDir = '.'; // 기본 디렉토리 경로
-    $currentDir = isset($_GET['dir']) ? $_GET['dir'] : $baseDir;
+    $baseDir = realpath('.'); // 기본 디렉토리의 실제 경로
+    $currentDir = isset($_GET['dir']) ? realpath($baseDir . '/' . $_GET['dir']) : $baseDir;
+
+    // 기본 디렉토리 밖으로 나가지 않도록 제한
+    if ($currentDir === false || strpos($currentDir, $baseDir) !== 0) {
+        $currentDir = $baseDir;
+    }
 
     // 상위 폴더로 이동 링크
     if ($currentDir !== $baseDir) {
@@ -82,7 +87,7 @@
     echo '<ul class="list-group">';
 
     $scanned_directory = array_diff(scandir($currentDir), array('..', '.'));
-    
+
     // index.php와 index.html 파일을 숨기기 위한 필터링
     $scanned_directory = array_filter($scanned_directory, function($item) use ($currentDir) {
         return !in_array($item, array('index.php', 'index.html'));
@@ -91,23 +96,25 @@
     foreach ($scanned_directory as $item) {
         $path = $currentDir . '/' . $item;
         $encodedPath = urlencode($path);
+        $safeItem = htmlspecialchars($item, ENT_QUOTES, 'UTF-8'); // XSS 방지용
+
         if (is_dir($path)) {
             echo '<li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
                     <div>
-                        <i class="fas fa-folder"></i><a href="?dir=' . $encodedPath . '">' . htmlspecialchars($item) . '</a>
+                        <i class="fas fa-folder"></i><a href="?dir=' . urlencode($path) . '">' . $safeItem . '</a>
                     </div>
                   </li>';
         } else {
             echo '<li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
                     <div>
-                        <i class="fas fa-file"></i><a href="' . htmlspecialchars($path) . '" download>' . htmlspecialchars($item) . '</a>
+                        <i class="fas fa-file"></i><a href="' . htmlspecialchars($path, ENT_QUOTES, 'UTF-8') . '" download>' . $safeItem . '</a>
                     </div>
                     <div class="dropdown">
                         <button class="btn btn-link dropdown-toggle text-dark" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="fas fa-ellipsis-h"></i>
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <li><button class="dropdown-item" onclick="copyDownloadLink(\'' . htmlspecialchars($path) . '\')">다운로드 링크 복사</button></li>
+                            <li><button class="dropdown-item" onclick="copyDownloadLink(\'' . htmlspecialchars($path, ENT_QUOTES, 'UTF-8') . '\')">다운로드 링크 복사</button></li>
                         </ul>
                     </div>
                   </li>';
@@ -122,7 +129,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
 <script>
     function copyDownloadLink(link) {
-        const url = window.location.origin + '/' + link;
+        const url = window.location.origin + '/' + encodeURI(link);
         navigator.clipboard.writeText(url).then(function() {
             alert('다운로드 링크가 복사되었습니다: ' + url);
         }, function(err) {
